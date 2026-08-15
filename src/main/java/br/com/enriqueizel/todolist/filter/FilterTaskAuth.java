dad6b0e81ba42590b11c2e0706a94e5f5ec7f461
enrique-izel-sns <eleaoizel@gmail.com>
2026-08-15 16:19:27 -0400

    feat: cria validação

--- src/main/java/br/com/enriqueizel/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/enriqueizel/todolist/filter/FilterTaskAuth.java
@@ -1,24 +1,56 @@
 package br.com.enriqueizel.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.enriqueizel.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        
-        System.out.println("Chegou no filtro");
-        chain.doFilter(request, response);
-    }
-    
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+                var servletPath = request.getServletPath();
+
+                if (servletPath.equals("/tasks/")) {
+                    var authorization = request.getHeader("Authorization");
+
+                    var user_auth = authorization.substring("Basic".length()).trim();
+
+                    byte[] authDecode = Base64.getDecoder().decode(user_auth);
+                    var authString = new String(authDecode);
+
+                    String[] credentials = authString.split(":");
+                    String userName = credentials[0];
+                    String password = credentials[1];
+
+                    var user = this.userRepository.findByUserName(userName);
+                    if (user == null) {
+                        response.sendError(401, "Usuário sem autorização");
+                    } else {
+                        var passwdVerified = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                        if (passwdVerified.verified) {
+                            filterChain.doFilter(request, response);
+                        } else {
+                            response.sendError(401);
+                        }
+                    }
+                } else {
+                    filterChain.doFilter(request, response);
+                }
+            }
 }
